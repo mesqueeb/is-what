28075a87238b3e7a2387d6b93899ac603bebf367
Adam Dorling <adam.x.dorling@gmail.com>
2018-09-28 10:30:40 +0100

    :bug:fixed typescript definitions to use `is` definition

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
 
-function getType (payload: any): string {
+function getType(payload: any): string {
   return Object.prototype.toString.call(payload).slice(8, -1)
 }
-function isUndefined (payload: any): boolean {
+function isUndefined(payload: any): payload is undefined {
   return getType(payload) === 'Undefined'
 }
-function isNull (payload: any): boolean {
+function isNull(payload: any): payload is null {
   return getType(payload) === 'Null'
 }
-function isObject (payload: any): boolean {
+function isObject(payload: any): payload is object {
   return getType(payload) === 'Object'
 }
-function isFunction (payload: any): boolean {
+function isFunction(payload: any): payload is Function {
   return getType(payload) === 'Function'
 }
-function isArray (payload: any): boolean {
+function isArray(payload: any): payload is any[] {
   return getType(payload) === 'Array'
 }
-function isString (payload: any): boolean {
+function isString(payload: any): payload is string {
   return getType(payload) === 'String'
 }
-function isNumber (payload: any): boolean {
+function isNumber(payload: any): payload is number {
   return (getType(payload) === 'Number' && !isNaN(payload))
 }
-function isBoolean (payload: any): boolean {
+function isBoolean(payload: any): payload is boolean {
   return getType(payload) === 'Boolean'
 }
-function isRegExp (payload: any): boolean {
+function isRegExp(payload: any): payload is RegExp {
   return getType(payload) === 'RegExp'
 }
-function isDate (payload: any): boolean {
+function isDate(payload: any): payload is Date {
   return (getType(payload) === 'Date' && !isNaN(payload))
 }
 
