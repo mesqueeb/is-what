86c412b3821c144fef95c2f15051050a7d23ccdb
mesqueeb <mesqueeb@users.noreply.github.com>
2023-05-20 13:17:26 +0900

    fix: try to simplify build

--- scripts/build.js
+++ scripts/build.js
@@ -5,34 +5,12 @@ import typescript from 'rollup-plugin-typescript2'
 
 const pkg = require('../package.json')
 
-function emitCjsPackageScope() {
-  return {
-    name: 'emit-cjs-package-scope',
-    generateBundle() {
-      this.emitFile({
-        fileName: 'package.json',
-        source: JSON.stringify({ type: 'commonjs' }, null, 2) + '\n',
-        type: 'asset',
-      })
-    },
-  }
+export default {
+  input: 'src/index.ts',
+  output: [
+    { file: pkg.exports['.'].require.default, format: 'cjs' },
+    { file: pkg.exports['.'].import.default, format: 'esm' },
+  ],
+  plugins: [typescript({ tsconfigOverride: { exclude: ['test/**/*'] } })],
+  external: Object.keys(pkg.dependencies || []),
 }
-
-const createConfig = ({ output, plugins = [] }) => {
-  return {
-    input: 'src/index.ts',
-    output,
-    external: Object.keys(pkg.dependencies || []),
-    plugins: [typescript({ tsconfigOverride: { exclude: ['test/**/*'] } }), ...plugins],
-  }
-}
-
-export default [
-  createConfig({
-    output: { file: pkg.main, format: 'cjs' },
-    plugins: [emitCjsPackageScope()],
-  }),
-  createConfig({
-    output: { file: pkg.module, format: 'esm' },
-  }),
-]
