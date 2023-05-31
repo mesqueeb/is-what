c83fc527bf0603bd678bc47a4556254cbd77c9ea
mesqueeb <mesqueeb@users.noreply.github.com>
2023-05-31 10:17:11 +0900

    feat: add JSDocs

--- src/index.ts
+++ src/index.ts
@@ -359,21 +359,65 @@ export function isPrimitive(
  */
 export const isNullOrUndefined = isOneOf(isNull, isUndefined)
 
+/**
+ * A factory function that creates a function to check if the payload is one of the given types.
+ * @example
+ * import { isOneOf, isNull, isUndefined } from 'is-what'
+ *
+ * const isNullOrUndefined = isOneOf(isNull, isUndefined)
+ *
+ * isNullOrUndefined(null) // true
+ * isNullOrUndefined(undefined) // true
+ * isNullOrUndefined(123) // false
+ */
 export function isOneOf<A, B extends A, C extends A>(
   a: TypeGuard<A, B>,
   b: TypeGuard<A, C>
 ): TypeGuard<A, B | C>
+/**
+ * A factory function that creates a function to check if the payload is one of the given types.
+ * @example
+ * import { isOneOf, isNull, isUndefined } from 'is-what'
+ *
+ * const isNullOrUndefined = isOneOf(isNull, isUndefined)
+ *
+ * isNullOrUndefined(null) // true
+ * isNullOrUndefined(undefined) // true
+ * isNullOrUndefined(123) // false
+ */
 export function isOneOf<A, B extends A, C extends A, D extends A>(
   a: TypeGuard<A, B>,
   b: TypeGuard<A, C>,
   c: TypeGuard<A, D>
 ): TypeGuard<A, B | C | D>
+/**
+ * A factory function that creates a function to check if the payload is one of the given types.
+ * @example
+ * import { isOneOf, isNull, isUndefined } from 'is-what'
+ *
+ * const isNullOrUndefined = isOneOf(isNull, isUndefined)
+ *
+ * isNullOrUndefined(null) // true
+ * isNullOrUndefined(undefined) // true
+ * isNullOrUndefined(123) // false
+ */
 export function isOneOf<A, B extends A, C extends A, D extends A, E extends A>(
   a: TypeGuard<A, B>,
   b: TypeGuard<A, C>,
   c: TypeGuard<A, D>,
   d: TypeGuard<A, E>
 ): TypeGuard<A, B | C | D | E>
+/**
+ * A factory function that creates a function to check if the payload is one of the given types.
+ * @example
+ * import { isOneOf, isNull, isUndefined } from 'is-what'
+ *
+ * const isNullOrUndefined = isOneOf(isNull, isUndefined)
+ *
+ * isNullOrUndefined(null) // true
+ * isNullOrUndefined(undefined) // true
+ * isNullOrUndefined(123) // false
+ */
 export function isOneOf<A, B extends A, C extends A, D extends A, E extends A, F extends A>(
   a: TypeGuard<A, B>,
   b: TypeGuard<A, C>,
@@ -381,6 +425,17 @@ export function isOneOf<A, B extends A, C extends A, D extends A, E extends A, F
   d: TypeGuard<A, E>,
   e: TypeGuard<A, F>
 ): TypeGuard<A, B | C | D | E | F>
+/**
+ * A factory function that creates a function to check if the payload is one of the given types.
+ * @example
+ * import { isOneOf, isNull, isUndefined } from 'is-what'
+ *
+ * const isNullOrUndefined = isOneOf(isNull, isUndefined)
+ *
+ * isNullOrUndefined(null) // true
+ * isNullOrUndefined(undefined) // true
+ * isNullOrUndefined(123) // false
+ */
 export function isOneOf(
   a: AnyFunction,
   b: AnyFunction,
