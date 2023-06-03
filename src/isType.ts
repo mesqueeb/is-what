97ca8065b088d43aeb8d93122fe1851fee583347
Jacob Hummer <jcbhmr@outlook.com>
2023-06-02 22:59:48 -0500

    Fix

--- src/isType.ts
+++ src/isType.ts
@@ -5,31 +5,35 @@ type AnyClass<A extends any[] = any[], R extends object = object> = new (
 ) => R;
 
 /**
- * Checks that the given value is **exactly** the given type (with some exceptions across realms).
- * This is equivalent to `x?.constructor === type || Object.prototype.toString.call(x).slice(8, -1)
- * === type.name`.
+ * Checks that the given value is **exactly** the given type (with some
+ * exceptions across realms). This is equivalent to `x?.constructor === type ||
+ * Object.prototype.toString.call(x).slice(8, -1) === type.name`.
  *
- * ℹ This _works_ across realms, but it's not perfect. For instance, user-defined classes will use
- * the default `Symbol.toStringTag` of `"Object"` which means that
- * `isType(iframe.contentWindow.hello, Object)` will _return `true`_ even though
- * `iframe.contentWindow.hello` is actually a `Hello` instance from another realm! This is a
- * limitation of the `Symbol.toStringTag` system. If possible, it's recommended to use one of the
- * more specific type-checking functions like `isFile()` or `isBlob()` from this `is-what` package.
+ * ℹ This _works_ across realms, but it's not perfect. For instance,
+ * user-defined classes will use the default `Symbol.toStringTag` of `"Object"`
+ * which means that `isType(iframe.contentWindow.hello, Object)` will _return
+ * `true`_ even though `iframe.contentWindow.hello` is actually a `Hello`
+ * instance from another realm! This is a limitation of the `Symbol.toStringTag`
+ * system. If possible, it's recommended to use one of the more specific
+ * type-checking functions like `isFile()` or `isBlob()` from this `is-what`
+ * package.
  *
- * ⚠️ This check will return `true` for objects that were created with `Object.create()` but weren't
- * initialized!
+ * ⚠️ This check will return `true` for objects that were created with
+ * `Object.create()` but weren't initialized!
  *
  * @example
  *   class Hello {}
- *   console.log(isType(new Hello(), Hello))
+ *   console.log(isType(new Hello(), Hello));
  *   //=> true
- *   console.log(isType(Object.create(Hello.prototype), Hello))
+ *   console.log(isType(Object.create(Hello.prototype), Hello));
  *   //=> true
- *   console.log(isType({}, Hello))
+ *   console.log(isType({}, Hello));
  *   //=> false
- *   console.log(isType(new Hello(), Object))
+ *   console.log(isType(new Hello(), Object));
  *   //=> false
- *   console.log(isType({}, Object))
+ *   console.log(isType({}, Object));
+ *   //=> true
+ *   console.log(isType(1, Number));
  *   //=> true
  *
  * @throws {TypeError} Will throw a `TypeError` if `type` is not a class
@@ -38,13 +42,15 @@ function isType<T extends AnyFunction | AnyClass>(x: unknown, type: T): x is T {
   if (typeof type !== "function" || !Object.hasOwn(type, "prototype")) {
     throw new TypeError(`${type} is not a class`);
   }
-  return (
-    !!x &&
-    (typeof x === "object" || typeof x === "function") &&
-    (x instanceof Object
-      ? x.constructor === type
-      : Object.prototype.toString.call(x).slice(8, -1) === type.name)
-  );
+  return x &&
+    (x instanceof Object ||
+      typeof x === "bigint" ||
+      typeof x === "boolean" ||
+      typeof x === "number" ||
+      typeof x === "string" ||
+      typeof x === "symbol")
+    ? Object.getPrototypeOf(x) === type.prototype
+    : Object.prototype.toString.call(x).slice(8, -1) === type.name;
 }
 
 export default isType;
