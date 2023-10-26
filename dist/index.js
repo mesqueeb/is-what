0a85ee28f654bfcd2a062382447ee2fb38318df1
mesqueeb <mesqueeb@users.noreply.github.com>
2023-10-26 13:56:06 +0900

    chore: build

--- dist/index.js
+++ dist/index.js
@@ -1,109 +1,74 @@
 function getType(payload) {
   return Object.prototype.toString.call(payload).slice(8, -1);
 }
-function isUndefined(payload) {
-  return getType(payload) === "Undefined";
+
+function isAnyObject(payload) {
+  return getType(payload) === "Object";
 }
-function isNull(payload) {
-  return getType(payload) === "Null";
+
+function isArray(payload) {
+  return getType(payload) === "Array";
 }
+
+function isBlob(payload) {
+  return getType(payload) === "Blob";
+}
+
+function isBoolean(payload) {
+  return getType(payload) === "Boolean";
+}
+
+function isDate(payload) {
+  return getType(payload) === "Date" && !isNaN(payload);
+}
+
+function isEmptyArray(payload) {
+  return isArray(payload) && payload.length === 0;
+}
+
 function isPlainObject(payload) {
   if (getType(payload) !== "Object")
     return false;
   const prototype = Object.getPrototypeOf(payload);
   return !!prototype && prototype.constructor === Object && prototype === Object.prototype;
 }
-function isObject(payload) {
-  return isPlainObject(payload);
-}
+
 function isEmptyObject(payload) {
   return isPlainObject(payload) && Object.keys(payload).length === 0;
 }
-function isFullObject(payload) {
-  return isPlainObject(payload) && Object.keys(payload).length > 0;
-}
-function isAnyObject(payload) {
-  return getType(payload) === "Object";
-}
-function isObjectLike(payload) {
-  return isAnyObject(payload);
-}
-function isFunction(payload) {
-  return typeof payload === "function";
-}
-function isArray(payload) {
-  return getType(payload) === "Array";
-}
-function isFullArray(payload) {
-  return isArray(payload) && payload.length > 0;
-}
-function isEmptyArray(payload) {
-  return isArray(payload) && payload.length === 0;
-}
-function isString(payload) {
-  return getType(payload) === "String";
-}
-function isFullString(payload) {
-  return isString(payload) && payload !== "";
-}
+
 function isEmptyString(payload) {
   return payload === "";
 }
-function isNumber(payload) {
-  return getType(payload) === "Number" && !isNaN(payload);
+
+function isError(payload) {
+  return getType(payload) === "Error" || payload instanceof Error;
 }
-function isPositiveNumber(payload) {
-  return isNumber(payload) && payload > 0;
-}
-function isNegativeNumber(payload) {
-  return isNumber(payload) && payload < 0;
-}
-function isBoolean(payload) {
-  return getType(payload) === "Boolean";
-}
-function isRegExp(payload) {
-  return getType(payload) === "RegExp";
-}
-function isMap(payload) {
-  return getType(payload) === "Map";
-}
-function isWeakMap(payload) {
-  return getType(payload) === "WeakMap";
-}
-function isSet(payload) {
-  return getType(payload) === "Set";
-}
-function isWeakSet(payload) {
-  return getType(payload) === "WeakSet";
-}
-function isSymbol(payload) {
-  return getType(payload) === "Symbol";
-}
-function isDate(payload) {
-  return getType(payload) === "Date" && !isNaN(payload);
-}
-function isBlob(payload) {
-  return getType(payload) === "Blob";
-}
+
 function isFile(payload) {
   return getType(payload) === "File";
 }
-function isPromise(payload) {
-  return getType(payload) === "Promise";
+
+function isFullArray(payload) {
+  return isArray(payload) && payload.length > 0;
 }
-function isError(payload) {
-  return getType(payload) === "Error";
+
+function isFullObject(payload) {
+  return isPlainObject(payload) && Object.keys(payload).length > 0;
 }
-function isNaNValue(payload) {
-  return getType(payload) === "Number" && isNaN(payload);
+
+function isString(payload) {
+  return getType(payload) === "String";
 }
-function isPrimitive(payload) {
-  return isBoolean(payload) || isNull(payload) || isUndefined(payload) || isNumber(payload) || isString(payload) || isSymbol(payload);
+
+function isFullString(payload) {
+  return isString(payload) && payload !== "";
 }
-const isNullOrUndefined = isOneOf(isNull, isUndefined);
-function isOneOf(a, b, c, d, e) {
-  return (value) => a(value) || b(value) || !!c && c(value) || !!d && d(value) || !!e && e(value);
+
+function isFunction(payload) {
+  return typeof payload === "function";
 }
+
 function isType(payload, type) {
   if (!(type instanceof Function)) {
     throw new TypeError("Type must be a function");
@@ -114,6 +79,7 @@ function isType(payload, type) {
   const name = type.name;
   return getType(payload) === name || Boolean(payload && payload.constructor === type);
 }
+
 function isInstanceOf(value, classOrClassName) {
   if (typeof classOrClassName === "function") {
     for (let p = value; p; p = Object.getPrototypeOf(p)) {
@@ -130,6 +96,76 @@ function isInstanceOf(value, classOrClassName) {
     }
     return false;
   }
+}
+
+function isMap(payload) {
+  return getType(payload) === "Map";
+}
+
+function isNaNValue(payload) {
+  return getType(payload) === "Number" && isNaN(payload);
+}
+
+function isNumber(payload) {
+  return getType(payload) === "Number" && !isNaN(payload);
+}
+
+function isNegativeNumber(payload) {
+  return isNumber(payload) && payload < 0;
+}
+
+function isNull(payload) {
+  return getType(payload) === "Null";
+}
+
+function isOneOf(a, b, c, d, e) {
+  return (value) => a(value) || b(value) || !!c && c(value) || !!d && d(value) || !!e && e(value);
+}
+
+function isUndefined(payload) {
+  return getType(payload) === "Undefined";
+}
+
+const isNullOrUndefined = isOneOf(isNull, isUndefined);
+
+function isObject(payload) {
+  return isPlainObject(payload);
+}
+
+function isObjectLike(payload) {
+  return isAnyObject(payload);
+}
+
+function isPositiveNumber(payload) {
+  return isNumber(payload) && payload > 0;
+}
+
+function isSymbol(payload) {
+  return getType(payload) === "Symbol";
+}
+
+function isPrimitive(payload) {
+  return isBoolean(payload) || isNull(payload) || isUndefined(payload) || isNumber(payload) || isString(payload) || isSymbol(payload);
+}
+
+function isPromise(payload) {
+  return getType(payload) === "Promise";
+}
+
+function isRegExp(payload) {
+  return getType(payload) === "RegExp";
+}
+
+function isSet(payload) {
+  return getType(payload) === "Set";
+}
+
+function isWeakMap(payload) {
+  return getType(payload) === "WeakMap";
+}
+
+function isWeakSet(payload) {
+  return getType(payload) === "WeakSet";
 }
 
 export { getType, isAnyObject, isArray, isBlob, isBoolean, isDate, isEmptyArray, isEmptyObject, isEmptyString, isError, isFile, isFullArray, isFullObject, isFullString, isFunction, isInstanceOf, isMap, isNaNValue, isNegativeNumber, isNull, isNullOrUndefined, isNumber, isObject, isObjectLike, isOneOf, isPlainObject, isPositiveNumber, isPrimitive, isPromise, isRegExp, isSet, isString, isSymbol, isType, isUndefined, isWeakMap, isWeakSet };
