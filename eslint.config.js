312ffc386d9509f13f3227053f19f9393a1edae3
mesqueeb <mesqueeb@users.noreply.github.com>
2024-06-07 13:55:03 +0900

    lint: allow any with warning

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,10 @@
 import config from '@cycraft/eslint/config'
 
-export default config
+export default [
+  ...config,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+]
