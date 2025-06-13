9b05f6593869d3c6e77daccfec7d3e098c72f1a8
Luca Ban <mesqueeb@users.noreply.github.com>
2025-06-14 07:40:40 +1200

    chore: build

--- dist/isObject.d.ts
+++ dist/isObject.d.ts
@@ -1,4 +1,4 @@
-import { PlainObject } from './isPlainObject.js';
+import { type PlainObject } from './isPlainObject.js';
 /**
  * Returns whether the payload is a plain JavaScript object (excluding special classes or objects
  * with other prototypes)
