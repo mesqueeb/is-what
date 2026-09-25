5292b5b00de066ca7dc28ee02cbc4c9c22828dfd
Luca Ban <mesqueeb@users.noreply.github.com>
2026-09-26 10:24:44 +1200

    chore: publish to jsr as well, explicit type on isNullOrUndefined for jsr, declare typescript as devDependency

--- src/isNullOrUndefined.ts
+++ src/isNullOrUndefined.ts
@@ -3,4 +3,4 @@ import { /* tree-shaking no-side-effects-when-called */ isOneOf } from './isOneO
 import { isUndefined } from './isUndefined.js'
 
 /** Returns true whether the payload is null or undefined */
-export const isNullOrUndefined = isOneOf(isNull, isUndefined)
+export const isNullOrUndefined: (payload: unknown) => payload is null | undefined = isOneOf(isNull, isUndefined)
